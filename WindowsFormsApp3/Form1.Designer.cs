ee281dcf781bf7a7d4201d4ebf3f0c404fe53233
rbuhian <r.buhian@tritontek.ph>
2018-11-26 10:19:31 +0800

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cmdPrint = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cmdPrintToFile = new System.Windows.Forms.Button();
+            this.cmdSelect = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(873, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Move NC1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "To";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(873, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // cmdPrint
+            // 
+            this.cmdPrint.Location = new System.Drawing.Point(236, 103);
+            this.cmdPrint.Name = "cmdPrint";
+            this.cmdPrint.Size = new System.Drawing.Size(130, 23);
+            this.cmdPrint.TabIndex = 5;
+            this.cmdPrint.Text = "Print Drawing";
+            this.cmdPrint.UseVisualStyleBackColor = true;
+            this.cmdPrint.Click += new System.EventHandler(this.cmdPrint_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Get Env Variable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(497, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Get Advance Option";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cmdPrintToFile
+            // 
+            this.cmdPrintToFile.Location = new System.Drawing.Point(659, 103);
+            this.cmdPrintToFile.Name = "cmdPrintToFile";
+            this.cmdPrintToFile.Size = new System.Drawing.Size(130, 23);
+            this.cmdPrintToFile.TabIndex = 8;
+            this.cmdPrintToFile.Text = "Print To File";
+            this.cmdPrintToFile.UseVisualStyleBackColor = true;
+            this.cmdPrintToFile.Click += new System.EventHandler(this.cmdPrintToFile_Click);
+            // 
+            // cmdSelect
+            // 
+            this.cmdSelect.Location = new System.Drawing.Point(800, 103);
+            this.cmdSelect.Name = "cmdSelect";
+            this.cmdSelect.Size = new System.Drawing.Size(130, 23);
+            this.cmdSelect.TabIndex = 9;
+            this.cmdSelect.Text = "Select Drawing";
+            this.cmdSelect.UseVisualStyleBackColor = true;
+            this.cmdSelect.Click += new System.EventHandler(this.cmdSelect_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(83, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Create directory";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 209);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.cmdSelect);
+            this.Controls.Add(this.cmdPrintToFile);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cmdPrint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button cmdPrint;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button cmdPrintToFile;
+        private System.Windows.Forms.Button cmdSelect;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Tekla.Structures;
+using Tekla.Structures.Dialog.UIControls;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                string dirName = new DirectoryInfo(textBox1.Text).Name;
+                DirectoryInfo parentDir = Directory.GetParent(textBox1.Text);
+                Directory.Delete(textBox1.Text,true);
+                string newDirectory = Path.Combine(parentDir.FullName, "CNC", "NC1");
+                Directory.CreateDirectory(newDirectory);
+
+                //Copy all *.nc1 files created in the CNC folder.
+                string[] files = Directory.GetFiles(parentDir.FullName, "*.nc1");
+                foreach (var item in files)
+                {
+                    string fileName = Path.GetFileName(item);
+                    fileName = fileName.Replace(dirName, string.Empty);
+                    File.Move(item, Path.Combine(newDirectory, fileName));
+                }
+                //Directory.EnumerateFiles(parentDir.FullName, "*.nc1").ToList().ForEach(x => File.Move(x, Path.Combine(newDirectory, Path.GetFileName(x))));
+            }
+        }
+
+        private void cmdPrint_Click(object sender, EventArgs e)
+        {
+            DrawingHandler drawingHandler = new DrawingHandler();
+            PrintAttributes printAttributes = new PrintAttributes();
+
+            printAttributes.Scale = 1;
+            printAttributes.PrintToMultipleSheet = false;
+            printAttributes.NumberOfCopies = 1;
+            printAttributes.Orientation = DotPrintOrientationType.Auto;
+            printAttributes.PrintArea = DotPrintAreaType.EntireDrawing;
+            printAttributes.ScalingType = DotPrintScalingType.Auto;
+            printAttributes.PrinterInstance = "PDFCreator";
+            
+
+            DrawingEnumerator selectedDrawings = drawingHandler.GetDrawingSelector().GetSelected();
+            while (selectedDrawings.MoveNext())
+            {
+
+                Drawing currentDrawing = selectedDrawings.Current;
+
+                // Make sure it is a valid drawing
+                if (currentDrawing != null)
+                {
+                    // Check that the drawing is up-to-date before printing
+                    if (currentDrawing.UpToDateStatus == DrawingUpToDateStatus.DrawingIsUpToDate)
+                    {
+                        
+                        drawingHandler.PrintDrawing(currentDrawing, printAttributes);
+                    }
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = EnvironmentVariables.GetEnvironmentVariable(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                var strValue = string.Empty;
+                bool result = Tekla.Structures.TeklaStructuresSettings.GetAdvancedOption(textBox1.Text, ref strValue);
+                MessageBox.Show(strValue);
+            }
+        }
+
+
+        private string PrintToFile(Drawing currentDrawing)
+        {
+            string DrawingType = GetDrawingTypeCharacter(currentDrawing);
+            string output_file = GetOutputFile(DrawingType);
+            string converted_file = ConvertOutputFile(output_file, currentDrawing);
+            
+
+            return converted_file;
+        }
+
+        private string ConvertOutputFile(string output_file, Drawing currentDrawing)
+        {
+            string convertedFile = string.Empty;
+            string drawingName = RemoveBrackets(currentDrawing.Mark);
+            string drawingTitle = currentDrawing.Name;
+
+            convertedFile = drawingName + " - " + drawingTitle;
+
+            return convertedFile;
+        }
+
+        private static string GetOutputFile(string DrawingType)
+        {
+            string output_file = string.Empty;
+
+            switch (DrawingType)
+            {
+                case "A": //Assembly Drawing
+                    output_file = EnvironmentVariables.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_A");
+                    break;
+                case "W": //Single Part Drawing
+                    output_file = EnvironmentVariables.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_W");
+                    break;
+                case "C": //CastUnit Drawing
+                    output_file = EnvironmentVariables.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_C");
+                    break;
+                case "M": //MultiDrawing
+                    output_file = EnvironmentVariables.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_M");
+                    break;
+                case "G": //GADrawing
+                    output_file = EnvironmentVariables.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_G");
+                    break;
+                default:
+                    output_file = "Unknown File";
+                    break;
+            }
+
+            return output_file;
+        }
+
+        private string GetDrawingTypeCharacter(Drawing DrawingInstance)
+        {
+            string Result = "U"; // Unknown drawing
+
+            if (DrawingInstance is GADrawing)
+            {
+                Result = "G";
+            }
+            else if (DrawingInstance is AssemblyDrawing)
+            {
+                Result = "A";
+            }
+            else if (DrawingInstance is CastUnitDrawing)
+            {
+                Result = "C";
+            }
+            else if (DrawingInstance is MultiDrawing)
+            {
+                Result = "M";
+            }
+            else if (DrawingInstance is SinglePartDrawing)
+            {
+                Result = "W";
+            }
+
+            return Result;
+        }
+
+        private void cmdPrintToFile_Click(object sender, EventArgs e)
+        {
+            DrawingHandler MyDrawingHandler = new DrawingHandler();
+            if (MyDrawingHandler.GetConnectionStatus())
+            {
+                DrawingEnumerator SelectedDrawings = MyDrawingHandler.GetDrawingSelector().GetSelected();
+                while (SelectedDrawings.MoveNext())
+                {
+                    Drawing currentDrawing = SelectedDrawings.Current;
+                    //Identifier =
+                    //            (Identifier)drawing.GetType().GetProperty("Identifier", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(drawing, null)
+
+                    PrintToFile(currentDrawing);
+                }
+            }
+        }
+
+        private string RemoveBrackets(string inputString)
+        {
+            StringBuilder builder = new StringBuilder(inputString);
+            builder.Replace("[", "");
+            builder.Replace("]", "");
+
+            return builder.ToString();
+        }
+
+        private void SelectDrawingsWithRevision()
+        {
+            DrawingHandler drawingHandler = new DrawingHandler();
+            Model teklaModel = new Model();
+            PrintAttributes printAttributes = new PrintAttributes();
+            printAttributes.Scale = 1;
+            printAttributes.PrintToMultipleSheet = false;
+            printAttributes.NumberOfCopies = 1;
+            printAttributes.Orientation = DotPrintOrientationType.Auto;
+            printAttributes.PrintArea = DotPrintAreaType.EntireDrawing;
+            printAttributes.ScalingType = DotPrintScalingType.Auto;
+            printAttributes.PrinterInstance = "PDFCreator";
+
+            string path = "C:\\temp\\opex\\OFA\\New folder";
+
+
+            if (teklaModel.GetConnectionStatus())
+            {
+                
+
+                List<int> drawingWithRevision = new List<int>();
+                DrawingEnumerator drawingsEnum = drawingHandler.GetDrawings();
+
+                while (drawingsEnum.MoveNext())
+                {
+                    int revision = 0;
+                    var drawing = drawingsEnum.Current as Drawing;
+                    Beam DWG = new Beam
+                    {
+                        Identifier =
+                                (Identifier)drawing.GetType().GetProperty("Identifier", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(drawing, null)
+                    };
+
+                    DWG.GetReportProperty("REVISION.NUMBER", ref revision);
+                    string DrawingType = GetDrawingTypeCharacter(drawing);
+
+                    //Should have revision and DrawingType is not SinglePart "W"
+                    if ((revision > 0) && (DrawingType != "W"))
+                    {
+                        DWG.Select();
+                        string filename = Path.Combine(path, PrintToFile(drawing) + ".pdf");
+                        drawingHandler.PrintDrawing(drawing, printAttributes, filename);
+                    }
+
+
+                }
+
+
+                string defaultReport = "FabTrol_Drawing_List-v30.xsr";
+                
+                string template = "450   FabTrol Drawing List-v30";
+                string fullPath = Path.Combine(path, defaultReport);
+                Operation.CreateReportFromSelected(template, fullPath, "Drawing List", "", "");
+            }
+        }
+
+        private void cmdSelect_Click(object sender, EventArgs e)
+        {
+            SelectDrawingsWithRevision();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = "C:\\temp\\opex\\OFA\\test";
+            MessageBox.Show(path, textBox1.Text);
+            string fullPath = Path.Combine(path, textBox1.Text);
+            MessageBox.Show(fullPath.ToString());
+        }
+    }
+}
